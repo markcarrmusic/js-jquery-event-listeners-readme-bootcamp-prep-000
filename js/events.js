9a822df2577f981de6a13f24d37503b1389b0477
Mark Carr <markcarrmusic@gmail.com>
2018-05-08 19:09:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,17 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(key) {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').prepend('')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
